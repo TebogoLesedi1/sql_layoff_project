711c63319d3b7a84d7a32695f3ee172e12ee2601
TebogoLesedi1 <leseditebogo22@gmail.com>
2026-04-24 01:13:19 +0200

    feat: implement ROW_NUMBER and PARTITION BY to identify duplicate records

--- sql_files/layoffs_staging.sql
+++ sql_files/layoffs_staging.sql
@@ -11,4 +11,29 @@ insert INTO layoffs_staging
 select *
 from layoffs_clean;
 
+--1. try ID duplicates if row_num is 2 or more there is duplicates
+select *, 
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, 'date'
+ORDER BY company) AS row_num
+from layoffs_staging;
+
+--create a CTE
+WITH duplicates_cte AS
+(
+select *, 
+ROW_NUMBER() OVER(
+PARTITION BY company,'location', industry, total_laid_off, percentage_laid_off, 'date',
+stage, country, funds_raised_millions
+ORDER BY company) AS row_num
+from layoffs_staging
+)
+SELECT * 
+FROM duplicates_cte
+WHERE row_num > 1;
+
+select *
+from layoffs_staging
+where company = 'casper';
+
 
